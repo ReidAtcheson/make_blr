7556ab3efa3f6a4b24556d96c060a33fa399a3c5
Reid Atcheson <reid.atcheson@gmail.com>
2022-07-17 13:16:54 -0500

    checkpoint

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from jax import random
 import jax.numpy as jnp
 import jax.nn as jnn
 from jax import grad, jit, vmap
+from jax.lax import fori_loop
 from functools import partial
 import optax
 
@@ -45,48 +46,67 @@ def make_block_precon(A,blocksize):
 #Create a preconditioner by blocking up
 #range and putting a rank-`d`-update
 #in each block and full matrices
-#on diagonal 
+#on diagonal.
+#Each block has the form:
+#(is_diagonal,params)
+#where is_diagonal indicates a diagonal block
 def make_blr(A,blocksize,d=1):
     key=random.PRNGKey(0)
     A=sp.lil_matrix(A)
     m,_=A.shape
-    blocks={}
+    blockVs=[]
+    blockUs=[]
+    Ds=[]
     for i in range(0,m,blocksize):
-        blocks[i]={}
+        ki=min(i+blocksize,m)-i
+        keys=random.split(key,2)
+        D.append(random.normal(keys[0],(ki,ki)))
+        key=keys[-1]
         for j in range(0,m,blocksize):
+            kj=min(j+blocksize,m)-j
+            keys=random.split(key,3)
+            Vs.append(random.normal(keys[0],(d,kj)))
+            Us.append(random.normal(keys[1],(ki,d)))
+            key=keys[-1]
+
+
+        for j,_ in enumerate(range(0,m,blocksize)):
             if i==j:
                 ids=list(range(i,min(i+blocksize,m)))
-                blocks[i][j]=jnp.array(np.linalg.inv(A[np.ix_(ids,ids)].toarray()))
+                blocks[i].append((True,jnp.array(np.linalg.inv(A[np.ix_(ids,ids)].toarray()))))
             else:
                 ki=min(i+blocksize,m)-i
                 kj=min(j+blocksize,m)-j
                 keys=random.split(key,3)
-                blocks[i][j]=(random.normal(keys[0],(ki,d)),random.normal(keys[1],(d,kj)))
+                blocks[i].append((False,(random.normal(keys[0],(ki,d)),random.normal(keys[1],(d,kj)))))
                 key=keys[-1]
     return blocks
 
-@partial(jit, static_argnums=[1,2])
-def eval_blr(blocks,m,blocksize,x):
-    ids=list(range(blocksize,m,blocksize))
-    xs=jnp.vsplit(x,ids)
-    out=[]
-    for i in range(0,m,blocksize):
-        def body(j):
-            if i==j:
-                D=blocks[i][j]
-                return D@xs[j//blocksize]
-            else:
-                U,Vt=blocks[i][j]
-                return U@(Vt@xs[j//blocksize])
-        col = [body(j) for j in range(0,m,blocksize)]
-        sumop=[jnp.eye(u.shape[0]) for u in col]
-        out.append(jnp.hstack(sumop)@jnp.vstack(col))
-    return jnp.vstack(out)
 
 @partial(jit, static_argnums=[1,2])
-def loss(params,m,blocksize,Ax,x):
-    blrx=eval_blr(params,m,blocksize,Ax)
-    return jnp.sum( (blrx-x)*(blrx-x) )/m
+def eval_blr(blocks,m,blocksize,x):
+    nblocks=len(blocks)
+    ids=list(range(blocksize,m,blocksize))
+    xs=jnp.vsplit(x,ids)
+
+    def row(brow):
+
+        def while_cond(
+
+
+
+
+
+    return jnp.concatenate([row(brow) for brow in blocks],axis=0)
+
+
+
+
+@partial(jit, static_argnums=[1,2])
+def loss(params,m,blocksize,Axs,xs):
+    blrxs=eval_blr(params,m,blocksize,Axs)
+    return jnp.sum([jnp.sum((blrx-x)*(blrx-x)) for blrx,x in zip(blrxs,xs)])
+
 
 
 
@@ -121,16 +141,21 @@ for it in range(nepochs):
     print("NEW SUBITERATIONS")
     x=rng.normal(size=(m,batchsize))
     Ax=A@x
+    ids=list(range(blocksize,m,blocksize))
+    xs=jnp.vsplit(x,ids)
+    Axs=jnp.vsplit(Ax,ids)
+
     for i in range(0,100):
         start=time.time()
-        g = grad(loss)(blr,m,blocksize,Ax,x)
+        g = grad(loss)(blr,m,blocksize,Axs,xs)
         updates,opt_state = opt.update(g,opt_state)
         blr = optax.apply_updates(blr,updates)
-        err=loss(blr,m,blocksize,Ax,x)
+        err=loss(blr,m,blocksize,Axs,xs)
         stop=time.time()
 
         print(f"it = {it},  elapsed = {stop-start : .4f}, loss = {err}")
-        losses.append(err)
+        if i==0:
+            losses.append(err)
 
 
 f=open("blr.dat","wb")
